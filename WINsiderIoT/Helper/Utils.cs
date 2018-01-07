02162474d69a90a1efc8ca7d9ec65aefec76ccc5
Sebastian Jensen <apps@tsjdev-apps.de>
2018-01-07 11:31:32 +0100

    - move feedurl to utils class

--- WINsiderIoT/Helper/Utils.cs
+++ WINsiderIoT/Helper/Utils.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace WINsiderIoT.Helper
 {
     public static class Utils
     {
-
+        public const string FeedUrl = "http://windowscommunity.de/feed/";
     }
 }
